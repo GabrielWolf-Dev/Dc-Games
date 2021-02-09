bd76df34f5f342fa315ead08b3e4cee2e8fc96da
Gabriel Wolf <65361452+GabrielWolf-Dev@users.noreply.github.com>
2021-02-08 22:50:20 -0300

    Refatoração e SEO
    Refatorando o script menu mobile e implementando tags SEO.

--- js/menuMobile.js
+++ js/menuMobile.js
@@ -1,16 +1,14 @@
-let screen_menu = document.getElementById('screen-menu');
-let icon_mobile = document.getElementById('icon-mobile');
+const screen_menu = document.getElementById('screen-menu');
+const icon_mobile = document.getElementById('icon-mobile');
 
-let linksMenu = {
+const linksMenu = {
    sobre: document.getElementById('sobre'),
    contato: document.getElementById('contato')
 };
+const { sobre, contato } = linksMenu;
 
-let {sobre, contato} = linksMenu;
-
-let linkMenuAtivo = (evento1, evento2) => {
-
-   let eventos = [evento1, evento2];
+function linkMenuAtivo(evento1, evento2) {
+   const eventos = [evento1, evento2];
 
    eventos.forEach(evento => {
       evento.addEventListener('click', () => {
@@ -18,29 +16,15 @@ let linkMenuAtivo = (evento1, evento2) => {
          icon_mobile.style.display = 'block'; 
       });
    });
-   
 }
-
 
 function openMenu() {
-
-   screen_menu.style.animation = 'animOpen 1s 1';
-   screen_menu.style.display = 'block';
-   icon_mobile.style.display = 'none';
+   screen_menu.classList.add('menuMobile--enable');
+   icon_mobile.classList.remove('iconMobile--enable');
    linkMenuAtivo(sobre, contato);
-
-}
-
-
-let disappearMenu = () => {
-         
-   screen_menu.style.display='none';
-   icon_mobile.style.display = 'block';
-   
 }
 
 function closeMenu() {
-    
-   screen_menu.style.animation = 'animClose 1s 1';
-   setTimeout(disappearMenu, 500);
+   screen_menu.classList.remove('menuMobile--enable');
+   icon_mobile.classList.add('iconMobile--enable');
 }
